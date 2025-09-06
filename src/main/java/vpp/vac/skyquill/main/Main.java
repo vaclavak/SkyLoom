3c410d8a2f28cf103849353ae288f428e7395280
vaclavak <126817987+vaclavak@users.noreply.github.com>
2025-09-06 11:06:31 +0200

    Changed version values

--- src/main/java/vpp/vac/skyquill/main/Main.java
+++ src/main/java/vpp/vac/skyquill/main/Main.java
@@ -13,10 +13,10 @@ import vpp.vac.skyquill.util.Logger;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "skyquill";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "preview-1.0.0";
 	public static boolean DebugMode = true;
 	public static final String PREFIX = "[SKYQUILL] ";
-	public static final String DEV_VERSION = "A13";
+	public static final String DEV_VERSION = "A13-preview";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
